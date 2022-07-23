1bd58d25db2f163d3dcd1b0dc5121981573689dd
alikz <ali6316k@gmail.com>
2022-07-23 17:35:04 +0430

    first commitmi

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        title: Text('Budget ui'),
+      ),
+    );
+  }
+}
