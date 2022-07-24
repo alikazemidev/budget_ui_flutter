41133e3c44f912027f78d31c15ce3f2d7d367009
alikz <ali6316k@gmail.com>
2022-07-24 20:25:54 +0430

    created list card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:budget_ui/data/data.dart';
+import 'package:budget_ui/models/category_model.dart';
 import 'package:flutter/material.dart';
 
 import '../Widget/bar_chart.dart';
+import '../models/expense_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -44,24 +46,78 @@ class _HomePageState extends State<HomePage> {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (context, index) {
-                return Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 6,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: BarChart(expenses: weeklySpending),
-                );
+                if (index == 0) {
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 6,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: BarChart(expenses: weeklySpending),
+                  );
+                } else {
+                  final Category category = categories[index - 1];
+                  double totalAmountSpent = 0.0;
+                  category.expenses!.forEach(
+                    (expense) {
+                      totalAmountSpent += expense.cost!;
+                    },
+                  );
+                  return _buildCategory(category, totalAmountSpent);
+                }
               },
-              childCount: 1,
+              childCount: 1 + categories.length,
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategory(Category category, double totalAmountSpent) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      height: 100,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 2),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                category.name!,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                '\$${(category.maxAmount! - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount}',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
           ),
         ],
       ),
