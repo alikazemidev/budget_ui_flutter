6b13a950ae56e28cf6627f1a6a3d27a6de820ce8
alikz <ali6316k@gmail.com>
2022-07-24 19:30:12 +0430

    created bar

--- lib/Widget/bar_chart.dart
+++ lib/Widget/bar_chart.dart
@@ -6,8 +6,17 @@ class BarChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double mostExpensive = 0;
+    expenses!.forEach(
+      (double price) {
+        if (price > mostExpensive) {
+          mostExpensive = price;
+        }
+      },
+    );
     return Column(
       children: [
+        SizedBox(height: 20),
         Text(
           'Weekly Spending',
           style: TextStyle(
@@ -40,10 +49,49 @@ class BarChart extends StatelessWidget {
           ],
         ),
         SizedBox(height: 30),
-        Row(
-          children: [
-            Bar(lable: 'su', amountSpent: expenses![0], mostExpensive: 200),
-          ],
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Bar(
+                lable: 'Su',
+                amountSpent: expenses![0],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                lable: 'Mo',
+                amountSpent: expenses![1],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                lable: 'Tu',
+                amountSpent: expenses![2],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                lable: 'We',
+                amountSpent: expenses![3],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                lable: 'TH',
+                amountSpent: expenses![4],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                lable: 'Fr',
+                amountSpent: expenses![5],
+                mostExpensive: mostExpensive,
+              ),
+              Bar(
+                lable: 'Sa',
+                amountSpent: expenses![6],
+                mostExpensive: mostExpensive,
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -53,11 +101,42 @@ class BarChart extends StatelessWidget {
 class Bar extends StatelessWidget {
   final String? lable;
   final double? amountSpent, mostExpensive;
-
-  const Bar({super.key, this.lable, this.amountSpent, this.mostExpensive});
+  final double _maxBarHeight = 150;
+  const Bar({
+    super.key,
+    this.lable,
+    this.amountSpent,
+    this.mostExpensive,
+  });
   @override
   Widget build(BuildContext context) {
-    var barHeight;
-    return Text('');
+    final barHeight = amountSpent! / mostExpensive! * _maxBarHeight;
+    return Column(
+      children: [
+        Text(
+          '\$${amountSpent!.toStringAsFixed(2)}',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 6),
+        Container(
+          height: barHeight,
+          width: 18,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          lable!,
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+          ),
+        )
+      ],
+    );
   }
 }
