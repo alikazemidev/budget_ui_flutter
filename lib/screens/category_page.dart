89b036bf4a9b5aa634b6912e51500ad53f6df387
alikz <ali6316k@gmail.com>
2022-07-25 21:00:51 +0430

    complete the ui app

--- lib/screens/category_page.dart
+++ lib/screens/category_page.dart
@@ -1,4 +1,7 @@
+import 'package:budget_ui/Widget/radial_painter.dart';
+import 'package:budget_ui/helpers/color_helper.dart';
 import 'package:budget_ui/models/category_model.dart';
+import 'package:budget_ui/models/expense_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -14,6 +17,14 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category!.expenses!.forEach(
+      (Expense expense) {
+        totalAmountSpent += expense.cost!;
+      },
+    );
+    final double amountLeft = widget.category!.maxAmount! - totalAmountSpent;
+    final double precent = amountLeft / widget.category!.maxAmount!;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -49,10 +60,84 @@ class _CategoryScreenState extends State<CategoryScreen> {
                   ),
                 ],
               ),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey.shade200,
+                  lineColor: getColor(context, precent),
+                  percent: precent,
+                  width: 15,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)}/\$${widget.category!.maxAmount!.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
             ),
+            _buildExpenses()
           ],
         ),
       ),
+    );
+  }
+
+  _buildExpenses() {
+    List<Widget> expensesList = [];
+    widget.category!.expenses!.forEach(
+      (Expense expense) {
+        expensesList.add(
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            height: 80,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 6,
+                  offset: Offset(0, 2),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    expense.name!,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '-\$${expense.cost!.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+    return Column(
+      children: expensesList,
     );
   }
 }
